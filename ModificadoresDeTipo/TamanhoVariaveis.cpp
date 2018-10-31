a171e087ec60dfeb4f8291080dbf37b158708eda
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2018-10-31 14:31:10 -0300

    Add files via upload

---
+++ ModificadoresDeTipo/TamanhoVariaveis.cpp
@@ -0,0 +1,50 @@
+#include <limits>
+#include <iostream>
+
+int main()
+{
+	// \t insere uma tabulação 
+	std::cout << "Tipo\t\t Minimo\t\t Maximo\n\n";
+	
+	std::cout << "bool\t\t"
+		<< +std::numeric_limits<bool>::min() << '\t' << '\t'
+		<< +std::numeric_limits<bool>::max() << '\n';
+	std::cout << "char\t\t"
+		<< +std::numeric_limits<char>::min() << '\t' << '\t'
+		<< +std::numeric_limits<char>::max() << '\n';
+	std::cout << "unsigned char\t"
+		<< +std::numeric_limits<unsigned char>::min() << '\t' << '\t'
+		<< +std::numeric_limits<unsigned char>::max() << '\n';
+	std::cout << "short\t\t"
+		<< +std::numeric_limits<short>::min() << '\t' << '\t'
+		<< +std::numeric_limits<short>::max() << '\n';
+	std::cout << "unsigned short\t"
+		<< +std::numeric_limits<short int>::min() << '\t' << '\t'
+		<< +std::numeric_limits<short int>::max() << '\n';
+	std::cout << "int\t\t"
+		<< +std::numeric_limits<int>::min() << '\t' << '\t'
+		<< +std::numeric_limits<int>::max() << '\n';
+	std::cout << "unsigned int\t"
+		<< +std::numeric_limits<unsigned int>::min() << '\t' << '\t'
+		<< +std::numeric_limits<unsigned int>::max() << '\n';
+	std::cout << "long int\t"
+		<< +std::numeric_limits<long int>::min() << '\t' << '\t'
+		<< +std::numeric_limits<long int>::max() << '\n';
+	std::cout << "unsigned long int "
+		<< +std::numeric_limits<unsigned long int>::min() << '\t' << '\t'
+		<< +std::numeric_limits<unsigned long int>::max() << '\n';
+	std::cout << "long long int\t"
+		<< +std::numeric_limits<long long int>::min() << '\t' << '\t'
+		<< +std::numeric_limits<long long int>::max() << '\n';
+	std::cout << "float\t\t"
+		<< +std::numeric_limits<float>::min() << '\t' << '\t'
+		<< +std::numeric_limits<float>::max() << '\n';
+	std::cout << "double\t\t"
+		<< +std::numeric_limits<double>::min() << '\t' << '\t'
+		<< +std::numeric_limits<double>::max() << '\n';
+	std::cout << "long double\t"
+		<< +std::numeric_limits<long double>::min() << '\t' << '\t'
+		<< +std::numeric_limits<long double>::max() << '\n';
+	system("PAUSE");
+
+}
