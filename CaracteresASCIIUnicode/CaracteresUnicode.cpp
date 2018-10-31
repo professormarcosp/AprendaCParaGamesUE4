d9792c247fa5eda7583f4c75d43917b1a8d9944a
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2018-10-31 15:03:08 -0300

    Add files via upload

--- CaracteresASCIIUnicode/CaracteresUnicode.cpp
+++ CaracteresASCIIUnicode/CaracteresUnicode.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <fcntl.h>  
+#include <io.h>
 
+int main()
+
+{
+
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	std::wcout << L"\u00AE" << L" \u263A";
+	system("PAUSE");
+
+}
