9b50f75871a9c481e1bc367307aaadc051d891ff
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2018-10-31 14:30:52 -0300

    Add files via upload

--- ModificadoresDeTipo/Modificadores.cpp
+++ ModificadoresDeTipo/Modificadores.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+int main()
+{
+	std::cout << "Tamanho char : " << sizeof(char) << std::endl;
+	std::cout << "Tamanho int : " << sizeof(int) << std::endl;
+	std::cout << "Tamanho short int : " << sizeof(short int) << std::endl;
+	std::cout << "Tamanho long int : " << sizeof(long int) << std::endl;
+	std::cout << "Tamanho long long int : " << sizeof(long long int) << std::endl;
+	std::cout << "Tamanho float : " << sizeof(float) << std::endl;
+	std::cout << "Tamanho float : " << sizeof(long float) << std::endl;
+	std::cout << "Tamanho double : " << sizeof(double) << std::endl;
+	std::cout << "Tamanho long double : " << sizeof(long double) << std::endl;
+	std::cout << "Tamanho wchar_t : " << sizeof(wchar_t) << std::endl;
+	system("PAUSE");
+
+	return 0;
+}
