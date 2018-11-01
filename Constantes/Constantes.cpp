918cf354a4365b65ead4789657f951db85170831
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2018-11-01 11:30:22 -0300

    Add files via upload

--- Constantes/Constantes.cpp
+++ Constantes/Constantes.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+//Para usar #define precisa colocar aqui abaixo dos includes
+//#define NUM_VIDAS 10
+//Observe que não tem ; no final!!!
 
+int main()
+{
+	const int NUM_VIDAS = 10;
+	int TotalDeVidas;
+	TotalDeVidas = NUM_VIDAS - 1;
+	std::cout << "Total de Vidas: " << TotalDeVidas << "\n";
+	std::cout << "Valor Constante NUM_VIDAS: " << NUM_VIDAS << "\n";
+	std::cout << "Endereço de Memória de NUM_VIDAS: " << &NUM_VIDAS << "\n";
+	system("PAUSE");
+
+}
