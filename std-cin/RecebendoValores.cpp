632ae067377da27c3d793d5b767f0c4c126bff1c
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2018-11-05 08:54:32 -0300

    Add files via upload

--- std-cin/RecebendoValores.cpp
+++ std-cin/RecebendoValores.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+int main()
+{
+	int Numero01, Numero02;
+	std::cout << "Digite o primeiro numero: ";
+	std::cin >> Numero01;
+	std::cout << "Numero01 = " << Numero01 << std::endl;
+	std::cout << "Digite o segundo numero: ";
+	std::cin >> Numero02;
+	std::cout << "Numero02 = " << Numero02;
+	system("PAUSE");
+}
