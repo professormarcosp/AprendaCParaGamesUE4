414ccf6ad27e78f0a2dacb112156c807202e5809
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2018-11-12 14:17:41 -0300

    Add files via upload

--- OperadoresLogicos/OperadoresLogicos.cpp
+++ OperadoresLogicos/OperadoresLogicos.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <tchar.h>
 
+int main()
+{
+	_tsetlocale(LC_ALL, _T("portuguese"));
+	double Numero;
+	bool ResultadoLogico, ResultadoLogico2;
+	std::cout << "Digite um Numero: ";
+	std::cin >> Numero;
+	std::cout << "True = 1 e False = 0" << "\n";
+	//A variável booleana MaiorQue100 irá receber
+	//o valor lógico da comparação (Numero > 100)
+	// Numero é maior que cem? 
+	ResultadoLogico = ((Numero > 100) && (Numero != 0)) ;
+	std::cout << "O Resultado Lógico da Expressão ((Numero > 100) && (Numero != 0)) é: " << ResultadoLogico <<  std::endl;
+	ResultadoLogico2 = ((Numero == 100) || (Numero > 200)) && (Numero < 1000);
+	std::cout << "O Resultado Lógico da Expressão ((Numero == 100) || (Numero > 200)) && (Numero < 1000); é: " << ResultadoLogico2 << std::endl;
+	system("PAUSE");
+}
