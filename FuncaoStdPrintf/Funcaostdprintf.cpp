e40e9d8e50d4bfb1f262e45830fd2241e8c59579
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2018-11-12 17:54:33 -0300

    Add files via upload

--- FuncaoStdPrintf/Funcaostdprintf.cpp
+++ FuncaoStdPrintf/Funcaostdprintf.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <tchar.h>
+#include <string>
 
+int main()
+{
+	_tsetlocale(LC_ALL, _T("portuguese"));
+	//Você pode usar ( ) para iniciar variáveis com valores literais
+	//{  } é inicialização uniforme, permite tanto iniciar tanto valores literais (int, float, caractere, quanto valores escalares, vetores, enums etc.
+	int NumeroInteiro{ 12 };
+	float NumeroReal{ 34.56 };
+	char Caractere{ 'c' };
+	//String é um cadeia de caracteres, é um texto e deve estar entre aspas duplas "texto da string"
+	//Para declarar uma string usamos std::string
+	std::string Texto{ "Vou Aprender C++ e descobrir o lado sombrio da força!" };
+	//%d indica que você quer que seja colocado na string:
+	//"Valor Número Inteiro = %d " o valor da variável NumeroInteiro
+	std::printf("Valor Inteiro: %d - Real: %.2f - Caractere: %c - Texto: %s \n", NumeroInteiro, NumeroReal, Caractere, Texto.c_str());
+	std::cout << "Texto da String: " << Texto << "\n";
+
+	std::printf("O Valor do Caractere Digitado: %c na Tabela ASCII é: %d \n", Caractere, Caractere);
+	/*%d Número decimal inteiro (int).
+	%f	 Número decimal de ponto flutuante. serve tanto para float como para double.
+	%lf  Serve para double. (l-long , f-float - lf)
+	%c	 Caractere: imprime o caractere que tem o código ASCII correspondente ao valor dado.
+	%s	 Sequência de caracteres (string, em inglês).
+	*/
+
+	system("PAUSE");
+	return 0;
+}
