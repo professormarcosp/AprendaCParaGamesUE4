0e88fb61e99f2e2629c3cf4e87ee62778ac2e7d2
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2018-11-20 14:42:57 -0300

    Add files via upload

--- SeEntaoIfElse/SeEntaoIfElse.cpp
+++ SeEntaoIfElse/SeEntaoIfElse.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
 
+int main()
+{
+	bool bFezSol, bCarroPronto, bSalarioDepositado;
+	bool bAcesso;
+	std::string SenhaDeAcesso = "cmaismais";
+	std::string SenhaDigitada;
+
+	std::cout << "Digite a senha de acesso: ";
+	std::cin >> SenhaDigitada;
+
+	/*Se a senha digitada pelo usuário e armazenada na variável SenhaDigitada for igual a senha de acesso as instruções após o if serão executadas pois a condição foi verdadeira true 1 */
+	if (SenhaDigitada == SenhaDeAcesso)
+	{
+		std::cout << "\nAcesso Permitido!" << "\n";
+		//aqui aproveitamos para indicar que o acesso foi concedido
+		//através de outra variável booleana bAcesso
+		bAcesso = true;
+		system("PAUSE");
+	}
+	/*Se a condição for falsa então as isntruções do else serão executadas*/
+	else
+	{
+		std::cout << "\n Acesso Negado!!!" << "\n";
+		system("PAUSE");
+		exit(0); //sai do programa, força o fechamento da aplicação
+	}
+
+	
+}
