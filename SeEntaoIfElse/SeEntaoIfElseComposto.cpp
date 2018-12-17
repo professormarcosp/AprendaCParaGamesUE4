ebb78c0dc9a203a83fb9f90baa284c15e74c73fc
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2018-12-17 15:37:02 -0300

    Update SeEntaoIfElseComposto.cpp

--- SeEntaoIfElse/SeEntaoIfElseComposto.cpp
+++ SeEntaoIfElse/SeEntaoIfElseComposto.cpp
@@ -13,27 +13,27 @@ int main()
 	std::cout << "Digite a senha de acesso: ";
 	std::cin >> SenhaDigitada;
 
-	/*Se a senha digitada pelo usuário e armazenada na variável SenhaDigitada for igual a senha de acesso as instruções após o if serão executadas pois a condição foi verdadeira true 1 */
+	/*Se a senha digitada pelo usuÃ¡rio e armazenada na variÃ¡vel SenhaDigitada for igual a senha de acesso as instruÃ§Ãµes apÃ³s o if serÃ£o executadas pois a condiÃ§Ã£o foi verdadeira true 1 */
 	if (SenhaDigitada == SenhaDeAcesso)
 	{
 		std::cout << "\nAcesso Permitido!" << "\n";
 		//aqui aproveitamos para indicar que o acesso foi concedido
-		//através de outra variável booleana bAcesso
+		//atravÃ©s de outra variÃ¡vel booleana bAcesso
 		bAcesso = true;
 		//system("PAUSE");
 	}
-	/*Se a condição for falsa então as isntruções do else serão executadas*/
+	/*Se a condiÃ§Ã£o for falsa entÃ£o as isntruÃ§Ãµes do else serÃ£o executadas*/
 	else
 	{
 		std::cout << "\n Acesso Negado!!!" << "\n";
 		system("PAUSE");
-		exit(0); //sai do programa, força o fechamento da aplicação
+		exit(0); //sai do programa, forÃ§a o fechamento da aplicaÃ§Ã£o
 	}
 	bFezSol = false; 
 	bCarroPronto = false; 
 	bSalarioDepositado = false;
-	//Aqui podemos deixar apenas a variável
-	//Não é necessário == pois será buscado o valor de bAcesso e ficará assim:
+	//Aqui podemos deixar apenas a variÃ¡vel
+	//NÃ£o Ã© necessÃ¡rio == pois serÃ¡ buscado o valor de bAcesso e ficarÃ¡ assim:
 	//if (true) ou if(false)
 	if (bAcesso)
 	{
@@ -41,13 +41,15 @@ int main()
 		{
 			std::cout << "\nVai dar Praia!!!" << std::endl;
 		}
-		//Aqui será executado se a condição do if falhar e se
+		//Aqui serÃ¡ executado se a condiÃ§Ã£o do if falhar e se
 		//bSalarioDepositado for false
 		//Da mesma forma aqui
-		//Como bSalarioDeposito é falso !(não) Falso dará True e ai entra no else if
+		//Como bSalarioDeposito Ã© falso !(nÃ£o) Falso darÃ¡ True e ai entra no else if
 		else if (!bSalarioDepositado)
 		{
-			std::cout << "Não vai dá Praia, o salário também não foi Depositado" << "\n";
+			//corrigi o texto pois !bSalarioDepositado significa que agora ao invÃ©s de false serÃ¡ true
+			//logo o bSalarioDepositado virÃ¡ verdadeiro e desta forma o salÃ¡rio foi depositado
+			std::cout << "NÃ£o vai dÃ¡ Praia, mas salÃ¡rio tambÃ©m foi Depositado" << "\n";
 		}
 	}
 
