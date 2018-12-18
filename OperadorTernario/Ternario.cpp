e228cc5955d5969454fd94ee156dc339efad3d53
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2018-12-18 09:09:41 -0300

    Add files via upload

--- OperadorTernario/Ternario.cpp
+++ OperadorTernario/Ternario.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
 
+int main()
+{
+	double MaiorNumero, Numero01, Numero02;
+
+	std::cout << "Digite Primeiro Numero: ";
+	std::cin >> Numero01;
+	std::cout << "\nDigite Segundo Numero: ";
+	std::cin >> Numero02;
+
+	//Neste caso o operador ternario ? está sendo utilizado para
+	//Atribuir um valor a uma variável
+	//Se Numero01 for maior que Numero02 Maior numero recebe Valor de Numero01. Caso contrário(Senão) MaiorNumero Recebe Numero02
+	MaiorNumero = (Numero01 > Numero02) ? Numero01 : Numero02;
+	std::cout << "\nMaior Numero Digitado: " << MaiorNumero << "\n";
+	//Aqui o operador ? esta sendo utilizado para decidir que comando será executado. Se for True o primeiro std:: vai para tela
+	//Se for Falso o segundo std vai para a tela
+	(Numero01 > Numero02) ? std::cout << "\nNumero01 Maior que Numero02\n" : std::cout << "Numero02 Maior que Numero01\n";
+
+	system("PAUSE");
+}
