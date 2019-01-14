aec3c62c25fa15527e0db5bf3a5667d8e8dacfc9
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-01-13 23:27:06 -0300

    Add files via upload

---
+++ ComandoWhile/ImparesDeZeroA50.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+int main()
+{
+	int Numero;
+	Numero = 0;
+	//loop infitivo!
+	/* neste caso Numero nunca deixará de ser 0 e a
+	condição Numero <= 50 será sempre verdadeira!
+	Logo ficará sempre no While!!!*/
+	while (Numero <= 50)
+	{
+		if (Numero % 2 != 0)
+		{
+			std::cout << Numero << " ";
+
+		}
+		//De alguma forma você precisa incrementar a variável Numero para que ela chegue até 50
+		//Qual condição de Parada do loop?
+		Numero++;
+
+	}
+	system("PAUSE");
+	return 0;
+}
