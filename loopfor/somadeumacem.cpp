5b64e685847417d4275ee3c444b8af6889c16628
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-01-31 22:15:19 -0300

    Add files via upload

--- loopfor/somadeumacem.cpp
+++ loopfor/somadeumacem.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+int main()
+{
+	int soma = 0;
+	for (int num = 1; num <= 100; num++)
+	{
+		soma = soma + num;
+	}
+	std::cout << "\nA soma dos numeros de 1 a 100: " << soma << "\n";
+	system("PAUSE");
+	return 0;
+}
