2e0c5d3ec9ab605e19593387162af8930edb4f3b
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-03-06 22:35:32 -0300

    Add files via upload

--- funcoes/IntroFuncoes.cpp
+++ funcoes/IntroFuncoes.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+// O que a função vai retornar?
+//tipo de retorno da função
+int SomaAUmNumero(int Numero, int NumeroASerSomado)
+{
+	return Numero + NumeroASerSomado;
+}
+int main()
+{
+	int Numero, NumeroASerSomado;
+	std::cout << "Digite um numero: " << "\n";
+	std::cin >> Numero;
+	std::cout << "Digite o numero a ser somado: " << "\n";
+	std::cin >> NumeroASerSomado;
+	std::cout << "A soma deste numero:" << Numero << "Com o Numero:" << NumeroASerSomado << "Sera: " << SomaAUmNumero(Numero, NumeroASerSomado);
+	system("PAUSE");
+	return 0;
+}
