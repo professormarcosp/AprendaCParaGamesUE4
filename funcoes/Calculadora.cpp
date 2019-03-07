4388ead9865ce888d44ceff9f68cb342df447e41
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-03-07 17:24:11 -0300

    Add files via upload

---
+++ funcoes/Calculadora.cpp
@@ -0,0 +1,52 @@
+/* ENTRADA: dois números reais
+PROCESSAMENTO:funcoes que calculam soma, subtração, multiplicação e divisão
+SAÍDA:  (num1 + num2,  num1 – num2, num1 * num2, num1/num2)
+*/
+#include <iostream>
+//PROTÒTIPOS DAS FUNCOES
+float Soma(float num1, float num2);
+float Subtracao(float num1, float num2);
+float Multiplicacao(float num1, float num2);
+float Divisao(float num1, float num2);
+
+
+int main()
+{
+	int Num1, Num2;
+
+	std::cout << "\nDigite Primeiro Numero: ";
+	std::cin >> Num1;
+
+	std::cout << "\nDigite Segundo Numero: ";
+	std::cin >> Num2;
+
+	std::cout << "\n*** Resultado das operacoes***\n";
+	std::cout << Num1 << " e " << Num2 << "\n";
+	std::cout << "Soma: " << Soma(Num1, Num2) << "\n";
+	std::cout << "Substracao: " << Subtracao(Num1, Num2) << "\n";
+	std::cout << "Multiplicacao: " << Multiplicacao(Num1, Num2) << "\n";
+	std::cout << "Divisao: " << Divisao(Num1, Num2) << "\n";
+	   	  
+	system("PAUSE");
+	return 0;
+}
+
+float Soma(float num1, float num2)
+{
+	return num1 + num2;
+}
+
+float Subtracao(float num1, float num2)
+{
+	return num1 - num2;
+}
+
+float Multiplicacao(float num1, float num2)
+{
+	return num1 * num2;
+}
+
+float Divisao(float num1, float num2)
+{
+	return num1 / num2;
+}
