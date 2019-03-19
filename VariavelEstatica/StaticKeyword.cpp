69623097f9b77f07edaa5a272cf6df7a5cebfe61
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-03-19 16:29:52 -0300

    Add files via upload

--- VariavelEstatica/StaticKeyword.cpp
+++ VariavelEstatica/StaticKeyword.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+int ContadorChamadas()
+{
+	//Variavel estática: Significa que após a saída da função o valor desta variável não será perdido.
+	//static indica que este valor seja mantido mesmo após a função ser encerrada
+	static int NumChamadasDaFuncao = 0;
+	NumChamadasDaFuncao++;
+	return NumChamadasDaFuncao;
+}
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << ContadorChamadas() << std::endl;
+	}
+	system("PAUSE");
+	return 0;
+}
