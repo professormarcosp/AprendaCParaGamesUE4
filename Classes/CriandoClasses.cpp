5ebad9727cf1393dcbaff682c5589deef0c069e7
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-04-11 22:45:16 -0300

    Update CriandoClasses.cpp

--- Classes/CriandoClasses.cpp
+++ Classes/CriandoClasses.cpp
@@ -4,14 +4,16 @@ class Casa
 {
 	//Estes são os atributos, as propriedades da classe e são representados por variáveis.
 	//Estes atributos serão as características de cada objeto criado a partir desta classe
-	//Quando o programa estiver sendo executado e queremos saber qual é a situação do objeto em relação ao valor de seus atributos dizemos que queremos saber o ESTADO do objeto
+	//Quando o programa estiver sendo executado e queremos saber qual é a situação do objeto em relação 
+	//ao valor de seus atributos dizemos que queremos saber o ESTADO do objeto
 	int NumQuartos;
 	float Tamanho;
 	//No geral você coloca b na frente de uma variável bool para indicar que ela é do tipo booleana(bool)
 	bool bTemSuite;
 
 public:
-	//Estas são as operações da classe, ou métodos ou as funções que o objeto poderá executar quando for criado(instanciado) a partir desta classe.
+	//Estas são as operações da classe, ou métodos ou as funções que o objeto poderá executar quando 
+	//for criado(instanciado) a partir desta classe.
 	void MostrarTamanho();
 	int ObtenhaNumeroDeQuartos();
 	bool TemSuite();
