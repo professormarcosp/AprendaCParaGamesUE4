f31d933907df9c10590d4540fca7c088fb85dc73
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-04-19 01:01:57 -0300

    Add files via upload

---
+++ Classes/ProjetoBanco/Banco.cpp
@@ -0,0 +1,36 @@
+//Na função main() iremos instanciar objetos
+//desta forma ela precisa incluir as declarações da classe Conta
+#include "Conta.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	//Criamos um  objeto ContaCliente01 do tipo Conta
+	//Está alocado na Stack
+	Conta ContaCliente01;
+	//Criamos um  objeto ContaCliente02 do tipo Conta
+	//Está alocado na Stack
+	Conta ContaCliente02;
+
+	ContaCliente01.SetBanco("Bradesco");
+	ContaCliente01.SetAgencia(1234);
+	ContaCliente01.SetNumConta(9999);
+	ContaCliente01.SetTitular("Fulano");
+	ContaCliente01.Depositar(14690);
+	ContaCliente01.Sacar(1500);
+
+	ContaCliente02.SetBanco("Banco Do Brasil");
+	ContaCliente02.SetAgencia(5678);
+	ContaCliente02.SetNumConta(1111);
+	ContaCliente02.SetTitular("Beltrano");
+
+	//o objeto ContaCliente02 sérá enviado como argumento para a função transferir
+	ContaCliente01.Transferir(ContaCliente02, 2000);
+	ContaCliente02.Sacar(500);
+	ContaCliente02.Sacar(5000);
+
+
+	system("PAUSE");
+	return 0;
+}
