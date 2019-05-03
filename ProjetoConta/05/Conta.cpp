cadedda9d58d30684dc0b3009a3cb351118c84fc
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-05-03 13:20:35 -0300

    Update and rename Conta.h to Conta.cpp

---
+++ ProjetoConta/05/Conta.cpp
@@ -0,0 +1,146 @@
+//precisamos incluir o arquivo .h relativo a classe Conta
+//Para que este arquivo Conta.cpp possa "Enxergar" as declarações da classe Conta.
+#include "Conta.h"
+#include <iostream>
+#include <string>
+
+Conta::Conta()
+{
+	std::cout << "\nConstrutor default objeto endereco: " << this << " executado...\n";
+
+	this->Banco = "";
+	this->Agencia = 0;
+	this->NumConta = 0;
+	this->Titular = "";
+	this->Saldo = 0.0;
+}
+
+Conta::Conta(std::string Banco, int Agencia, int NumConta, std::string Titular, double Saldo)
+{
+	std::cout << "\nConstrutor objeto endereco: " << this << " executado...\n";
+	this->Banco = Banco;
+	this->Agencia = Agencia;
+	this->NumConta = NumConta;
+	this->Titular = Titular;
+	this->Saldo = Saldo;
+}
+
+
+Conta::~Conta()
+{
+	std::cout << "\nDestrutor objeto endereco: " << this << " executado...\n";
+}
+
+//Aqui o operador escopo:: está indicando que a função/Método sacar pertence a classe Conta e que este código a seguir vai implementar a função Sacar.
+bool Conta::Sacar(double Valor)
+{
+	//Se o valor do Saldo no Objeto for menor que o valor a ser sacado, ele não poderá sacar
+	if (Saldo < Valor)
+	{
+		std::cout << "\Saldo Insuficiente!\n";
+		std::cout << "Seu Saldo Atual: R$" << ConsultarSaldo() << "\n";
+		return false;
+	}
+	else
+	{
+	//Senão ele poderá sacar
+	//Temos que diminuir o saldo
+		Saldo = Saldo - Valor;
+		std::cout << "Seu Saldo Atual: R$" << ConsultarSaldo() << "\n";
+		return true;
+	}
+	
+}
+
+void Conta::Depositar(double Valor)
+{
+	Saldo += Valor; //o mesmo que Saldo = Saldo + Valor;
+	std::cout << "\nDeposito de R$ " << Valor << " Efetuado com Sucesso!\n";
+}
+
+//A função recebe como argumentos para seus seus parêmtros um Objeto do tipo Casa e um valor double.
+
+//Aqui o parâmetro Destino irá receber este objeto
+void Conta::Transferir(Conta &Destino, double Valor)
+{
+	if (Saldo < Valor)
+	{
+		std::cout << "\nSaldo Insuficiente\n";
+
+	}
+	else
+	{
+		//Aqui Destino está com o objeto passado para a função
+		//Logo vai chamar o método Depositar desta função
+		Destino.Depositar(Valor);
+		//Depois de depositar você deve retirar este valor depositado do saldo
+		Saldo -= Valor;
+		std::cout << "\n*****Dados*****\n";
+		std::cout << "Titular: " << Destino.GetTitular() << "\n";
+		std::cout << "Banco: " << Destino.GetBanco() << "\n";
+		std::cout << "Agencia: " << Destino.GetAgencia() << "\n";
+		std::cout << "Conta: " << Destino.GetNumConta() << "\n";
+		std::cout << "Transferência Realizada com Sucesso!\n";
+		std::cout << "Seu Saldo Atual: R$" << ConsultarSaldo() << "\n";
+		
+	}
+}
+
+double Conta::ConsultarSaldo()
+{
+	return Saldo;
+}
+
+std::string Conta::GetBanco()
+{
+	return Banco;
+}
+
+int Conta::GetAgencia()
+{
+	return Agencia;
+}
+
+int Conta::GetNumConta()
+{
+	return NumConta;
+}
+
+std::string Conta::GetTitular()
+{
+	return this->Titular;
+}
+
+void Conta::GetEnderecoThis()
+{
+	std::cout << "\nthis contem dentro dele o endereco: " << this << "\n";
+
+}
+
+void Conta::SetBanco(std::string Banco)
+{
+	//Como o compilador vai saber se Bancó é variável local ou o atributo da classe Conta?
+	//se this é um ponteiro para chegar no apontado pelo ponteiro temos
+	//*this , mas como this é um ponteiro para um objeto ele precisa usar a notação ponto. para acessar atributos e executar funções do objeto
+
+	//Agora o compilador sabe que Banco é a variável local que recebeu o argumento string
+	//e que deve ser colocada no atributo Banco do objeto apontado por this
+	//(*this).Banco = Banco;
+	//Contudo no geral usa-se a notação flecha
+	this->Banco = Banco;
+}
+
+void Conta::SetAgencia(int Agencia)
+{
+	Agencia = Agencia;
+}
+
+void Conta::SetNumConta(int NumConta)
+{
+	this->NumConta = NumConta;
+}
+
+void Conta::SetTitular(std::string Titular)
+{
+	this->Titular = Titular;
+}
