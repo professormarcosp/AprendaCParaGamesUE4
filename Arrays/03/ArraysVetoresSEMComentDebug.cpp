76e6fc806276edd91570762bcde1dcb98d3f08a5
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-05-08 08:23:15 -0300

    Create ArraysVetoresSEMComentDebug.cpp

---
+++ Arrays/03/ArraysVetoresSEMComentDebug.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	
+	//Você pode suprimir o número nos colchetes.
+	float Notas[]{ 0.0f, 1.0f, 2.0f, 3.0f, 4.0f }; // se quiser iniciar todos com zero pode ser assim com único zero 0.0
+	float Media, Soma{ 0.0 };
+	std::string NomeAluno;
+
+	std::cout << "\nDigite o nome do Aluno: ";
+	std::cin >> NomeAluno;
+	
+	for (int i = 0; i <= 4; i++)
+	{
+		std::cout << "\nDigite a nota" << (i+1)<< ": ";
+		std::cin >> Notas[i];
+		Soma = Soma + Notas[i];
+		system("CLS");
+	}
+	
+	Media = Soma/5;
+	
+	std::cout << "\nA Media das notas do aluno foi: " << Media << "\n";
+	
+	system("PAUSE");
+	return 0;
+}
