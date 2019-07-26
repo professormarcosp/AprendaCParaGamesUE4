8bdee37182a129b33c50d4b13dc1ce2af246b408
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-07-26 11:19:57 -0300

    Create StructLivroSemComentarios.cpp

---
+++ Structs/StructLivroSemComentarios.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <locale>
+
+struct Livro
+{
+	int ID;
+	std::string Titulo;
+	std::string Autor;
+	std::string Editora;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	struct Livro livro;
+	
+	std::cout << "***Cadastro de Livro***" << "\n";
+	std::cout << "Digite ID do Livro: ";
+	std::cin >> livro.ID;
+
+	std::cout << "\nDigite o Título do Livro: ";
+	
+	std::cin.ignore();
+	std::getline(std::cin, livro.Titulo);
+
+	std::cout << "\nDigite o Autor do Livro: ";
+	std::getline(std::cin, livro.Autor);
+
+	std::cout << "\nDigite A Editora do Livro: ";
+	std::getline(std::cin, livro.Editora);
+
+	std::cout << "ID: " << livro.ID << "\n";
+	std::cout << "Titulo: " << livro.Titulo << "\n";
+	std::cout << "Autor: " << livro.Autor << "\n";
+	std::cout << "Editora: " << livro.Editora << "\n";
+
+	system("PAUSE");
+	return 0;
+}
