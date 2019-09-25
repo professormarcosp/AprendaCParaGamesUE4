9d10bde623149ba50fdd2d5fc83f8e7d79b65d0e
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-09-25 15:32:12 -0300

    Create PrimeiraListaEx02b.cpp

---
+++ PrimeiraListaExercicio/PrimeiraListaEx02b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//b) Depois substitua o comando std::endl pelo cara ctere de escape nova linha \n New Line
+int main()
+{
+	std::cout
+		<< "Vou" << "\n"
+		<< "aprender" << "\n"
+		<< "a" << "\n"
+		<< "programar" << "\n"
+		<< "na" << std::endl
+		<< "Linguagem C++" << "\n";
+
+	system("PAUSE");
+	return 0;
+}
