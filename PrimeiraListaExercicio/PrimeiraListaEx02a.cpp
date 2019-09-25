1daf7095765fcac82541d0b5559c38d2a367b2ca
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-09-25 15:27:35 -0300

    Create PrimeiraListaEx02a.cpp

---
+++ PrimeiraListaExercicio/PrimeiraListaEx02a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	std::cout 
+		<< "Vou"  << std::endl 
+		<< "aprender" << std::endl 
+		<< "a" << std::endl 
+		<< "programar" << std::endl 
+		<< "na" << std::endl 
+		<< "Linguagem C++" << std::endl;
+
+	system("PAUSE");
+	return 0;
+}
