476964e19538d36bc1d31ca8a44f1b9583fb4965
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-09-25 15:54:38 -0300

    Create PrimeiraListaEx03.cpp

---
+++ PrimeiraListaExercicio/PrimeiraListaEx03.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+//Professor Marcos Pacheco
+//Data: 21/12/2122 :)
+
+/* O Programa a seguir coloca na tela a frase: Vou aprender a programar na
+   Linguagem C++. Contudo cada palavra será exibida em um única linha
+   através da inserção de quebra de linhas
+*/
+  
+int main()
+{
+	std::cout
+		<< "Vou" << "\n"
+		<< "aprender" << "\n"
+		<< "a" << "\n"
+		<< "programar" << "\n"
+		<< "na" << "\n"
+		<< "Linguagem C++" << "\n\n"
+		<< "Vou" << "\n"
+		<< "aprender" << "\n"
+		<< "a" << "\n"
+		<< "programar" << "\n"
+		<< "na" << "\n"
+		<< "Linguagem C++" << "\n";
+	   	  
+	system("PAUSE");
+	return 0;
+}
+
+
