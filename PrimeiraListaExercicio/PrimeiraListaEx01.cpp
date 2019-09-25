fee281f47c5e3b6aa7b4f02300005b22709213d2
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-09-25 15:23:07 -0300

    Create PrimeiraListaEx01.cpp

---
+++ PrimeiraListaExercicio/PrimeiraListaEx01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/*Desenvolva um programa que escreve na tela a frase:Vou aprender a programar na Linguagem C++*/
+
+int main()
+{
+	std::cout << "Vou aprender a programar na Linguagem C++" << std::endl;
+	system("PAUSE");
+	return 0;
+}
