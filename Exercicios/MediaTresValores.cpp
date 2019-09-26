87a66416c0e524bc507760eab14d39800bb9871b
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-09-26 12:53:30 -0300

    Create MediaTresValores.cpp

---
+++ Exercicios/MediaTresValores.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "portuguese");
+	float Num1, Num2, Num3;
+	float Media = 0.0;
+
+	std::cout << "Digite o Primeiro Número: ";
+	std::cin >> Num1;
+	std::cout << "Digite o Primeiro Número: ";
+	std::cin >> Num2;
+	std::cout << "Digite o Primeiro Número: ";
+	std::cin >> Num3;
+
+	Media = (Num1 + Num2 +  Num3) / 3;
+
+	std::cout << "Números Digitados: " << Num1 << " " << Num2 << " " << Num3 << "\n";
+	std::cout << "Média dos Números: " << Media << "\n";
+
+	return 0;
+} 
