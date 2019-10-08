e46d1d265373fb679782591811d9e0c38de62d8a
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-10-08 17:40:52 -0300

    Update ConceitoDeHeranca.cpp

--- Heranca/ConceitoDeHeranca.cpp
+++ Heranca/ConceitoDeHeranca.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+
 //Classe Base também chamada de Superclasse, Classe Mãe/Pai
 class Base
 {
-
-//As seções pública, protegida e privada de uma classe devem ser declaradas nessa ordem (a seção pública(public) é declarada antes da seção protegida(protected) que é declarada antes da seção privada(private) ).
+//As seções public, protegida e privada de uma classe devem ser declaradas nesta ordem: A seção public(pública) depois a seção protected(protegida) e depois a seção private(privada)
+//Indica que os membros do objeto criado a partir desta classe terão acesso direto a todos os atributos e funções presentes na seção public
 public:
 	void SolicitarNumeros()
 	{
@@ -11,32 +12,39 @@ public:
 		std::cin >> Num1;
 		std::cout << "Num2: ";
 		std::cin >> Num2;
+		//Chamada do método privado da classe dentro da própria classe o que é permitido
+		/*Ou seja não é necessário ter todos os metodos públicos. Neste caso apenas SolicitarNumeros é público e isso basta, pois este método irá chamar os demais métodos privados da classe*/
 		MostrarResultado(Num1, Num2);
 	}
-
+//Aqui indica que os membros da seção privada só serão acessíveis dentro do código da classe. O objeto não poderá acessar diretamente estes itens atributos e funções
 private:
 	float Num1, Num2;
 	float Soma(int Num1, int Num2)
 	{
 		return (Num1 + Num2);
 	}
+
+	//Veja que este método só poderá ser chamado(invocado) dentro do c´pdigo da classe
 	void MostrarResultado(int Num1, int Num2)
 	{
 		std::cout << "Soma de: " << Num1 << " + " << Num2 << " = " << Soma(Num1, Num2) << "\n";
 	}
-	
+
 };
 
-//Classe Derivada, também chamada de Subclasse, classe Filha/Filho
-class Derivada : public Base {};
-
+//Agora declaramos uma classe que herda da classe Base, porém ela não tem nada está vazia
+//A classe derivada também é chamada de subclasse, classe filha/filho
+class Derivada :public Base{};
 
 int main()
 {
 	
+	//Criamos um objeto a partir da classe Derivada
 	Derivada Obj;
 	Obj.SolicitarNumeros();
-	
+	/*Isso indica que uma classe herda tudo de sua classe Base(Superclasse) mas NÃO HERDA
+	OS ITENS PRIVADOS(PRIVATE) DA SUPERCLASSE(CLASSE BASE)*/
 	system("PAUSE");
+
 	return 0;
 }
