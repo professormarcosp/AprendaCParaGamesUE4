8a541822499329eae3ff6bd22cb2d7bb4b376c2d
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2019-11-28 10:56:34 -0300

    Update Matematica.h

--- CriandoHeaderFiles/Matematica.h
+++ CriandoHeaderFiles/Matematica.h
@@ -1,24 +1,24 @@
 #pragma once
-/*ENTRADA: dois números inteiros
-PROCESSAMENTO: operações matemáticas
-Soma dos dois números
-Subtração dos dois números
-Multiplicado dos dois números
-Divisão dos dois números
-Potencia dos dois números (primeiro número elevado pelo segundo número)
-Raiz quadrada dos dois números
-SAIDA: Exiba na tela os resultados de todas as operações também utilizando chamada de funções
-- cabeçalho Matematica.h  protótipos das funções 
--arquivo Matematica.cpp para ser a implementação do arquivo de
-cabeçalho Matematica.h 
-- arquivo ProgramaMatematico.cpp que conteráfunção main( ) e será o
+/*ENTRADA: dois nÃºmeros inteiros
+PROCESSAMENTO: operaÃ§Ãµes matemÃ¡ticas
+Soma dos dois nÃºmeros
+SubtraÃ§Ã£o dos dois nÃºmeros
+Multiplicado dos dois nÃºmeros
+DivisÃ£o dos dois nÃºmeros
+Potencia dos dois nÃºmeros (primeiro nÃºmero elevado pelo segundo nÃºmero)
+Raiz quadrada dos dois nÃºmeros
+SAIDA: Exiba na tela os resultados de todas as operaÃ§Ãµes tambÃ©m utilizando chamada de funÃ§Ãµes
+- cabeÃ§alho Matematica.h  protÃ³tipos das funÃ§Ãµes 
+-arquivo Matematica.cpp para ser a implementaÃ§Ã£o do arquivo de
+cabeÃ§alho Matematica.h 
+- arquivo ProgramaMatematico.cpp que conterÃ¡funÃ§Ã£o main( ) e serÃ¡ o
 ponto de partida deste programa.
 */
 int Soma(int num1, int num2);
 int Subtracao(int num1, int num2);
 int Multiplicacao(int num1, int num2);
 float Divisao(int num1, int num2);
-int Potencia(int num1, int num2);
+double Potencia(double num1, double num2);
 float RaizQuadrada(int num);
 
 
