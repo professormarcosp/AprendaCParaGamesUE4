9efa53a8c509c81342030d0fe613771ad4d769af
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2020-02-04 12:57:29 -0300

    Update ExibindoDataEHora.cpp

--- Structs/ExibindoDataEHora.cpp
+++ Structs/ExibindoDataEHora.cpp
@@ -10,33 +10,16 @@ int main()
 	/*Função localtime
 	 recebe tempo em segundos de uma variável do tipo time_t, converte para o tempo local, armazena os dados na struct e retorna um ponteiro para uma struct do tipo tm com os dados locais.*/
 	tm* MinhaHora = localtime(&HoraAtual);
-	std::cout << "\nHora: " << 1 + MinhaHora->tm_hour << ":" << 1 +
-		MinhaHora->tm_min << ":" << 1 + MinhaHora->tm_sec;
-	std::cout << "\nDia: " << MinhaHora->tm_mday;
-	std::cout << "\nMes: " << 1 + MinhaHora->tm_mon;
-	std::cout << "\nAno " << 1900 + MinhaHora->tm_year << "\n";
-
-	system("PAUSE");
-	return 0;
-}
-/* VERSÃO SEM HORÁRIO DE VERÃO
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <ctime>
-
-int main()
-{
-	//O tipo de dados time_t é usado para representar o tempo de calendário.
-	time_t HoraAtual = time(0);
-
-	/*Função localtime
-	 recebe tempo em segundos de uma variável do tipo time_t, converte para o tempo local, armazena os dados na struct e retorna um ponteiro para uma struct do tipo tm com os dados locais.
-	tm* MinhaHora = localtime(&HoraAtual);
+	/*ERRATA: não é necessário colocar 1 + para hora, minuto e segundo*/
 	std::cout << "\nHora: " << MinhaHora->tm_hour << ":" << MinhaHora->tm_min << ":" << MinhaHora->tm_sec;
 	std::cout << "\nDia: " << MinhaHora->tm_mday;
+	/*O horário de verão acabou logo não coloque mais 1 +*/
 	std::cout << "\nMes: " << 1 + MinhaHora->tm_mon;
 	std::cout << "\nAno " << 1900 + MinhaHora->tm_year << "\n";
-
+	//pressione enter duas vezes bem rápido para ver os resultados sincronizados
+	system("DATE");
+	system("TIME");
 	system("PAUSE");
+
 	return 0;
-}*/
+}
