8a79b2efb2e307a34f29440e215ef62fba81e373
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-09-08 11:40:49 -0300

    Update Modificadores.cpp

--- ModificadoresDeTipo/Modificadores.cpp
+++ ModificadoresDeTipo/Modificadores.cpp
@@ -8,7 +8,6 @@ int main()
 	std::cout << "Tamanho long int : " << sizeof(long int) << std::endl;
 	std::cout << "Tamanho long long int : " << sizeof(long long int) << std::endl;
 	std::cout << "Tamanho float : " << sizeof(float) << std::endl;
-	std::cout << "Tamanho float : " << sizeof(long float) << std::endl;
 	std::cout << "Tamanho double : " << sizeof(double) << std::endl;
 	std::cout << "Tamanho long double : " << sizeof(long double) << std::endl;
 	std::cout << "Tamanho wchar_t : " << sizeof(wchar_t) << std::endl;
